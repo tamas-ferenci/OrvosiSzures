8acc9721215daeae9be2fa03323abd3df7f623ea
tamas-ferenci <tamas.ferenci@medstat.hu>
2019-05-07 08:49:17 +0200

    Kiinduló változat.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# OrvosiSzures
-Egészségügyi szűrőprogramok, orvosi tesztek eredményességének vizsgálata
+* [Bevezetés és motiváció](#bevezetés-és-motiváció)
+* [A program használata](#a-program-használata)
+* [Megjegyzések](#megjegyzések)
+* [Verziótörténet](#verziótörténet)
+
+# Bevezetés és motiváció
+
+Az orvosi diagnosztika fontos eszközét a különféle tesztek jelentik. Itt most azokról a diagnosztikai eljárásokról lesz szó, melyek célja egy betegség fennállásának a megállapítása (tehát pl. nem az állapot súlyosságának a felmérése). Az egyszerűség kedvéért arra az esetre szorítkozunk, amikor egyetlen tesztről beszélünk (nem több teszt együttes eredménye alapján állapítja meg az orvos a diagnózist), és amikor a tesztnek csak két kimenete lehet, "beteg" vagy "egészséges" (nem lehet tehát kicsit vagy nagyon beteg az alany, és nem lehet kis vagy nagy valószínűséggel beteg, a döntés bináris). Ettől eltekintve a kérdést a legáltalánosabban értjük, tehát a "teszt" jelentheti akár egy tünet jelenlétét vagy egy panasz fennállását is.
+
+A fent leírt helyzetben a tesztnek kétféle hibája lehet: ha egy egészséges emberre azt mondja, hogy beteg, illetve ha egy betegre azt mondja, hogy egészséges. A kétféle hibázást az orvosi gyakorlatban két mutatóval szokták mérni:
+
+* a teszt *szenzitivitása* az, hogy egy beteg embert mekkora valószínűséggel minősít tényleg betegnek (azt méri, hogy mennyire veszi észre a betegséget, más szóval, hogy mennyire biztos, hogy egy egészségesnek minősített ember tényleg egészséges -- hiszen, ha a teszt nem szenzitív, akkor sok beteget is egészségesnek mond),
+* a teszt *specificitása* pedig az, hogy egy egészségest mekkora valószínűséggel minősít tényleg egészségesnek (azaz mennyire biztos, hogy a betegnek minősített tényleg beteg -- hiszen, ha egy teszt nem specifikus, akkor sok egészségest is betegnek minősít).
+
+Egy rendkívül fontos gondolat, amit nagyon sokszor elfelejtenek, hogy ez a kettő mutató nem mond semmit arról, ami klinikailag fontos! Ezek feltételes valószínűségek, amik azt mondják, hogy feltéve, hogy betegek vagyunk, mi egy adott teszteredmény valószínűsége. Ez persze fontos dolog, hiszen ez jellemzi, hogy a teszt mennyire működik jól, de a konkrét orvosi döntési helyzetben nem ez érdekes, hanem a fordított valószínűség: nem az érdekel minket, hogy ha az alany egészséges, akkor mekkora valószínűséggel negatív a teszt, hanem az, hogy ha negatív a teszt akkor mekkora valószínűséggel egészséges az alany! És hasonlóképp, nem az érdekes, hogy ha beteg, akkor mekkora valószínűséggel pozitív a teszt, hanem, hogy ha pozitív a teszt, akkor mekkora valószínűséggel beteg az alany. Ezek fontosak számunkra, ezek neve: *prediktív érték*, az előbbi a negatív, az utóbbi a pozitív prediktív érték.
+
+A dolog kiszámítása könnyen demonstrálható egy egyszerű számpéldán. Fontos is ezt látni, mert az eredmény meglepő lehet! Mondjuk, hogy a tesztünk szenzitivitása 90%, specificitása 99%. Tegyük fel továbbá, hogy a tesztelt alanyok körében minden ezredik beteg ténylegesen. Hogy kerekek legyenek a számok, vegyünk 100 ezer alanyt, akiket mind tesztelünk; ekkor közöttük 100 beteg és 99900 egészséges lesz. Az előbbiek közül a teszt 90-et fog betegnek és 10-et egészségesnek minősíteni (szenzitivitás), az utóbbiak közül 999-et betegnak és 98901-et egészségesnek (specificitás). Ezek alapján már megválaszolhatjuk a számunkra fontos kérdéseket. Mekkora a valószínűsége, hogy egy pozitív lelet azt jelenti, hogy tényleg betegek vagyunk? Annyi, amennyi a betegek aránya a pozitívnak minősítetteken belül! Számoljunk: a teszt pozitívnak minősít összesen 90+999=1089 alanyt (90-et helyesen, 999-et helytelenül), így a ténylegesen betegek aránya a betegnek minősítetteken belül: 90/1089=8,3%. Ez a teszt pozitív prediktív értéke ebben az esetben; hasonlóan kiszámolható lenne a negatív prediktív érték is.
+
+Azaz: noha a tesztünk igen kitűnő (90% szenzitivitás, 99% specificitás!), mégis, egy pozitív lelet kevesebb mint 10% valószínűséggel jelent ténylegesen betegséget! Ezen sok ember elcsodálkozik; a magyarázat egész egyszerűen az, hogy hajlamosak vagyunk elfelejteni azt, hogy mennyi a betegek aránya a tesztelt alanyokon belül. Ha ez alacsony, mint a fenti példában is, akkor hiába nagyon jó a teszt, a sokkal-sokkal több egészséges 1%-a még mindig több (téves) pozitív leletet fog adni, mint a kevés beteg 90%-a (helyesen). 
+
+Az egész fenti történet két köntösben is előadható. Vagy azt mondjuk, hogy tömegeket tesztelünk (népegészségügyi szűrés), ez esetben a teszt előtti valószínűség egész egyszerűen a betegség prevalenciája, tehát az, hogy az alanyok mekkora hányada beteg, vagy azt, hogy egyetlen embert tesztelünk, ez esetben a teszt előtti valószínűség az ő egyéni jellemzőiből, ismert adataiból fakadó valószínűsége a betegség fennállásának. A kettőt néha megkülönböztetve szokták tárgyalni, noha lényegében ugyanarról van szó.
+
+Az, hogy még kitűnő teszttel is alacsony lehet a pozitív prediktív érték, komolyan megfontolandó tényező ritka betegségek szűrésekor, de az előbbi megjegyzés mutatja, hogy pontosan ugyanez a helyzet akkor, ha egyetlen alanyt tesztelünk, de nála nagyon alacsony a betegség fennállásának a valószínűsége. (Szokták mondani, hogy "sebaj, az még mindig jobb, ha tévesen betegnek minősítjük, később úgyis kiderül, hogy nem az, de ha elnézzük a betegséget, abból nagy baj lehet". Ez sajnos nem ilyen egyszerű, hiszen a téves betegnek minősítés, anyagi és pszichológiai terhein túl, nagyon gyakran további, esetleg invazívabb vizsgálatokat von maga után, kezelés megkezdését egyáltalán nem mellékhatásmentes módszerekkel stb.) Természetesen a dolog fordítva is igaz: ha nagyon magas a teszt előtti valószínűség, akkor még egy negatív lelet is jelentheti azt, hogy további is igen valószínűen beteg az alany. Ezeket a szempontokat figyelembe kell venni egy teszt megrendeléséről való racionális döntés során (azon túl, természetesen, hogy az adott helyzetben mekkora súlya van a kétféle tévedésnek, ez persze orvosi kérdés, ami a helyzettől függ).
+
+# A program használata
+
+Láthattuk a fentiekből, hogy a prediktív értékeket három tényező határozza meg: a szenzitivitás, a specificitás és a tesztelés előtti valószínűség (pl. prevalencia). Szépen, áttekinthetően ábrázolni viszont egyszerre csak két dolgot lehet, ennek megfelelően az alkalmazás egy paraméter értékét bekéri (`Beállítandó változó`), és azt fixnek veszi. E paraméter értéke csúszkával, és az alatta lévő mezőbe a szám közvetlen beírásával egyaránt megadható (bármelyik kitöltése automatikusan frissíti a másikat). A két megjelenő ábra -- pozitív és negatív prediktív érték -- a másik két paraméter összes lehetséges kombinációjának fényében mutatja a megfelelő prediktív értékeket. Az ábra le is tölthető, PDF és PNG formátumban is.
+
+Az alkalmazásnak két füle van, a `Grafikon` az alapértelmezett, ekkor a fent leírt ábrák jelennek meg, de a `Számszerű adatok` fülre átkattintva megjelennek egy pont adatai számszerűen is (a pont a `Speciális beállítások` mezőt bepipálva válik beállíthatóvá, lásd következő részt).
+
+A `Speciális beállítások` mezőt bepipálva további beállítások válnak elérhetővé:
+
+* Megadhatóak, hogy az ábrán mik legyenek a tengelyek határai.
+* Megfordíthatóak a két tengely.
+* Logaritmikus beosztásúvá tehető mindkét tengely.
+* Lekérdezhető egy konkrét pont: bepipálva ezt a mezőt beállíthatóvá válik a pont helye, ez egyfelől megjelenik az ábrán, másfelől a korábban említett `Számszerű adatok` fülön megjelennek az ahhoz tartozó értékek.
+
+# Megjegyzések
+
+* Noha igyekeztem a lehető legalaposabban eljárni, a programhoz természetesen nincs garancia. Különösen most, hogy még a kezdeti fázisban van; pontosan emiatt viszont hálásan megköszönök minden tesztelést (kiemelten: ugyanazon számítások, vizualizációk más módon történő elvégzését, és ezek eredményének összevetését az én programom által szolgáltatottakkal).
+* Az előbbi cél érdekében a teljes munkám transzparens: ebben a GitHub repozitóriumban nyilvánosan elérhető tettem a teljes programot, [R szkript](app.R) formájában.
+* A hibaellenőrzésektől teljesen függetlenül is nagy örömmel veszek minden visszajelzést, javítási/bővítési/továbbfejlesztési ötletet!
+* Az alkalmazás `R` statisztikai környezet alatt `shiny` használatával készült, a vizualizációhoz `lattice` csomagot használtam.
+
+
+# Verziótörténet
+
+Verzió|Dátum|Kommentár
+------|-----|---------
+v1.00|2019-05-07|Kiinduló változat.
---
+++ app.R
@@ -0,0 +1,248 @@
+library( shiny )
+
+params <- list( pretest = list( min = 0.01, max = 100-0.01, step = 0.01, value = 10, label = "Teszt előtti valószínűség [%]" ),
+                sens = list( min = 0.1, max = 100-0.1, step = 0.1, value = 90, label = "Szenzitivitás [%]" ),
+                spec = list( min = 0.1, max = 100-0.1, step = 0.1, value = 99, label = "Specificitás [%]" ) )
+paramslim <- list( pretest = list( min = 0, max = 100, step = 0.01, value = c( 1, 20 ),
+                                   label = "Teszt előtti valószínűség tengely határai" ),
+                   sens = list( min = 0, max = 100, step = 0.1, value = c( 90, 100 ), label = "Szenzitivitás tengely határai" ),
+                   spec = list( min = 0, max = 100, step = 0.1, value = c( 90, 100 ), label = "Specificitás tengely határai" ) )
+
+ui <- fluidPage(
+    
+    theme = "owntheme.css",
+    
+    tags$head( 
+        tags$meta( name = "description", content = paste0( "Egészségügyi szűrőprogramok, orvosi tesztek ",
+                                                           "eredményességének vizsgálatát lehetővé tevő alkalmazás. ",
+                                                           "Írta: Ferenci Tamás." ) ),
+        tags$meta( property = "og:title", content = "Egészségügyi szűrőprogramok, orvosi tesztek eredményességének vizsgálata" ),
+        tags$meta( property = "og:type", content = "website" ),
+        tags$meta( property = "og:locale", content = "hu_HU" ),
+        tags$meta( property = "og:url",
+                   content = "http://research.physcon.uni-obuda.hu/OrvosiSzures/" ),
+        tags$meta( property = "og:image",
+                   content = "http://research.physcon.uni-obuda.hu/OrvosiSzures_Pelda.png" ),
+        tags$meta( property = "og:description", content = paste0( "Egészségügyi szűrőprogramok, orvosi tesztek ",
+                                                                  "eredményességének vizsgálatát lehetővé tevő alkalmazás. ",
+                                                                  "Írta: Ferenci Tamás." ) ),
+        tags$meta( name = "DC.Title", content = "Egészségügyi szűrőprogramok, orvosi tesztek eredményességének vizsgálata" ),
+        tags$meta( name = "DC.Creator", content = "Ferenci Tamás" ),
+        tags$meta( name = "DC.Subject", content = "orvosi szűrés" ),
+        tags$meta( name = "DC.Description", content = paste0( "Egészségügyi szűrőprogramok, orvosi tesztek ",
+                                                              "eredményességének vizsgálatát lehetővé tevő alkalmazás. " ) ),
+        tags$meta( name = "DC.Publisher",
+                   content = "http://research.physcon.uni-obuda.hu/OrvosiSzures/" ),
+        tags$meta( name = "DC.Contributor", content = "Ferenci Tamás" ),
+        tags$meta( name = "DC.Language", content = "hu_HU" )
+    ),
+    
+    tags$div( id="fb-root" ),
+    tags$script( HTML( "(function(d, s, id) {
+                     var js, fjs = d.getElementsByTagName(s)[0];
+                     if (d.getElementById(id)) return;
+                     js = d.createElement(s); js.id = id;
+                     js.src = 'https://connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v2.12';
+                     fjs.parentNode.insertBefore(js, fjs);
+                     }(document, 'script', 'facebook-jssdk'));" ) ),
+    
+    tags$style( ".shiny-file-input-progress {display: none}" ),
+    
+    titlePanel( "Egészségügyi szűrőprogramok, orvosi tesztek eredményességének vizsgálata" ),
+    
+    p( "A program használatát részletesen bemutató súgó, valamint a technikai részletek",
+       a( "itt", href = "https://github.com/tamas-ferenci/OrvosiSzures",
+          target = "_blank" ), "olvashatóak el." ),
+    div( class="fb-like",
+         "data-href"="http://research.physcon.uni-obuda.hu/OrvosiSzures/",
+         "data-layout"="standard", "data-action"="like", "data-size"="small",
+         "data-show-faces"="true", "data-share"="true"), p(),
+    
+    sidebarLayout(
+        sidebarPanel(
+            selectInput( "type", "Beállítandó változó", c( `Teszt előtti valószínűség` = "pretest",
+                                                           `Szenzitivitás` = "sens", `Specificitás` = "spec" ) ),
+            do.call( sliderInput, c( inputId = "slider", params$pretest ) ),
+            do.call( numericInput, c( inputId = "sliderNumeric", params$pretest[ names( params$pretest )!="label" ],
+                                      label = "" ) ),
+            downloadButton( "DownloadFigPDF", "Az ábra letöltése (PDF)" ),
+            downloadButton( "DownloadFigPNG", "Az ábra letöltése (PNG)" ),
+            checkboxInput( "advanced", "Speciális beállítások" ),
+            conditionalPanel( "input.advanced==1",
+                              do.call( sliderInput, c( inputId = "xlimit", paramslim$sens ) ),
+                              do.call( sliderInput, c( inputId = "ylimit", paramslim$spec ) ),
+                              checkboxInput( "flip", "Tengelyek megfordítása" ),
+                              checkboxInput( "xlog", "Vízszintes tengely logaritmikus" ),
+                              checkboxInput( "ylog", "Függőleges tengely logaritmikus" ),
+                              checkboxInput( "showloc", "Lekérdezett pont megjelenítése" ),
+                              conditionalPanel( "input.showloc==1",
+                                                numericInput( "xloc", "Lekérdezett szenzitivitás [%]", 95, 0, 100, 0.1 ),
+                                                numericInput( "yloc", "Lekérdezett specificitás [%]", 95, 0, 100, 0.1 ) ) )
+        ),
+        
+        mainPanel(
+            tabsetPanel(
+                tabPanel( "Grafikon", p(),
+                          p( paste0( "Minden orvosi tesztnek kétféle hibázása lehet: ha egy beteget egészségensek minősít, ",
+                                     "és ha egy egészségest betegnek. Ezeket, tehát a teszt jóságát jellemzi a szenzitivitás ",
+                                     "(annak a valószínűsége, hogy a teszt egy beteg alanyt betegnek minősít) és a ",
+                                     "specifitás (annak a valószínűsége, hogy egy egészségest tényleg egészségesnek). ",
+                                     "A teszt előtti valószínűség úgy értendő, hogy még a teszt elvégzése előtt mekkora ",
+                                     "annak a valószínűsége, hogy a tesztelt alany beteg (a teszteltek hány százaléka ",
+                                     "beteg ténylegesen); ez lehet például a betegség prevalenciája. A prediktív érték ",
+                                     "a klinikailag fontos mutató: azt mondja meg, hogy ha a lelet pozitív akkor mekkora ",
+                                     "valószínűséggel beteg tényleg az alany (pozitív prediktív érték), illetve ha a lelet ",
+                                     "negatív, akkor mekkora valószínűséggel egészséges tényleg (negatív prediktív érték).") ),
+                          plotOutput( "resultPlot" ) ),
+                tabPanel( "Számszerű adatok", tableOutput( "resultTable" ) )
+            )
+        )
+    ),
+    
+    h4( "Írta: Ferenci Tamás (Óbudai Egyetem, Élettani Szabályozások Kutatóközpont), v1.00" ),
+    
+    # tags$footer( h4( "Írta: Ferenci Tamás (Óbudai Egyetem, Élettani Szabályozások Kutatóközpont), v1.00" ),
+    #              style = "position:absolute; bottom:0; width:100%; height:50px; padding: 10px; z-index: 1000;" ),
+    
+    tags$script( HTML( "var sc_project=11601191; 
+                      var sc_invisible=1; 
+                      var sc_security=\"5a06c22d\";
+                      var scJsHost = ((\"https:\" == document.location.protocol) ?
+                      \"https://secure.\" : \"http://www.\");
+                      document.write(\"<sc\"+\"ript type='text/javascript' src='\" +
+                      scJsHost+
+                      \"statcounter.com/counter/counter.js'></\"+\"script>\");" ),
+                 type = "text/javascript" )
+)
+
+server <- function(input, output, session) {
+    
+    observeEvent( input$slider, {
+        if( input$slider!=input$sliderNumeric ) {
+            updateNumericInput( session, "sliderNumeric", value = input$slider )
+        }
+    })
+    
+    observeEvent( input$sliderNumeric, {
+        if( input$slider!=input$sliderNumeric & !is.na( input$sliderNumeric ) & is.numeric( input$sliderNumeric ) ) {
+            updateSliderInput( session, "slider", value = input$sliderNumeric )
+        }
+    })
+    
+    observeEvent( input$type, {
+        do.call( updateSliderInput, c( session = session, inputId = "slider", params[[ input$type ]] ) )
+        do.call( updateNumericInput, c( session = session, inputId = "sliderNumeric",
+                                        params[[ input$type ]][ names( params[[ input$type ]] )!="label" ], label = "" ) )
+        do.call( updateSliderInput, c( session = session, inputId = "xlimit", paramslim[ names( paramslim )!=input$type ][[1]] ) )
+        do.call( updateSliderInput, c( session = session, inputId = "ylimit", paramslim[ names( paramslim )!=input$type ][[2]] ) )
+    })
+    
+    observe({
+        updateNumericInput( session, "xloc", step = if( input$type=="pretest" ) 0.01 else 0.1,
+                            label = switch( input$type, "sens" = "Lekérdezett teszt előtti valószínűség [%]",
+                                            "pretest" = "Lekérdezett szenzitivitás [%]",
+                                            "spec" = "Lekérdezett teszt előtti valószínűség [%]" ) )
+        updateNumericInput( session, "yloc", step = if( input$type=="pretest" ) 0.01 else 0.1,
+                            label = switch( input$type, "spec" = "Lekérdezett szenzitivitás [%]",
+                                            "sens" = "Lekérdezett specificitás [%]",
+                                            "pretest" = "Lekérdezett specificitás [%]" ) )
+    })
+    
+    dat <- reactive({
+        dat <- list()
+        dat[ input$type ] <- input$slider
+        dat[[ names( paramslim )[ names( paramslim )!=input$type ][ 1 ] ]] <- seq( input$xlimit[ 1 ], input$xlimit[ 2 ],
+                                                                                   length.out = 100 )
+        dat[[ names( paramslim )[ names( paramslim )!=input$type ][ 2 ] ]] <- seq( input$ylimit[ 1 ], input$ylimit[ 2 ],
+                                                                                   length.out = 100 )
+        dat <- do.call( expand.grid, dat )
+        dat <- transform( dat,
+                          npv = ( spec*(100-pretest)/( (100-sens)*pretest + spec*(100-pretest) ) )*100,
+                          ppv = ( sens*pretest/( sens*pretest + (100-spec)*(100-pretest) ) )*100 )
+        dat
+    })
+    
+    plotInput <- function() {
+        
+        form <- switch( input$type,
+                        "pretest" = if ( input$flip )  "~spec+sens" else "~sens+spec",
+                        "sens" = if ( input$flip )  "~spec+pretest" else "~pretest+spec",
+                        "spec" = if ( input$flip )  "~sens+pretest" else "~pretest+sens" )
+        
+        do.call( gridExtra::grid.arrange, c( lapply( c( "ppv", "npv" ), function( var ) {
+            atpoints <- range( dat()[[ var ]], na.rm = TRUE )
+            atpoints <- seq( atpoints[ 1 ], atpoints[ 2 ], length.out = 1000 )
+            colrp <- colorRampPalette( c( "white",  "red" ) )( length( atpoints ) )
+            atcontour <- round( quantile( dat()[[ var ]], (1:9)/10, na.rm = TRUE ), 1 )
+            
+            lattice::contourplot(
+                as.formula( paste0( var, form ) ), data = dat(), colorkey = list( col = colrp, at = atpoints ),
+                xlab = switch( labels( terms( as.formula( form ) ) )[ 1 ], "pretest" = "Teszt előtti valószínűség [%]",
+                               sens = "Szenzitivitás [%]", "spec" = "Specificitás [%]" ),
+                ylab = switch( labels( terms( as.formula( form ) ) )[ 2 ], "pretest" = "Teszt előtti valószínűség [%]",
+                               sens = "Szenzitivitás [%]", "spec" = "Specificitás [%]" ),
+                main = if ( var == "ppv" ) "Pozitív prediktív érték" else "Negatív prediktív érték",
+                scales = list( x = list( log = input$xlog ), y = list( log = input$ylog ) ),
+                sub = list( paste0( "Beállított ", switch( input$type, "pretest" = "teszt előtti valószínűség",
+                                                           "sens" = "szenzitivitás", "spec" = "specificitás" ), ": ",
+                                    input$slider, "%" ), font = 3 ),
+                panel = function( ..., at, contour, region, labels, dat = dat() ) {
+                    lattice::panel.levelplot( ..., at = atpoints, col.regions = colrp, region = TRUE )
+                    lattice::panel.contourplot( ..., at = atcontour, contour = TRUE, region = FALSE,
+                                                labels = list( labels = gsub( ".", ",", atcontour, fixed = TRUE ),
+                                                               col = "blue" ) )
+                    if( input$showloc ) {
+                        lattice::panel.points( input$xloc, input$yloc, pch = 19, cex = 1.5 )
+                        lattice::panel.text( input$xloc, input$yloc,
+                                             round( list(... )$z[ intersect( which( abs( list(...)$x-input$xloc )==min(
+                                                 abs( list(...)$x-input$xloc ) ) ),
+                                                 which( abs( list(...)$y-input$yloc )==min(
+                                                     abs( list(...)$y-input$yloc ) ) ) ) ], 1 ),
+                                             pos = 3, col = "blue", cex = 1.5 )
+                        lattice::panel.lines( c( input$xloc, input$xloc ), c( 0, input$yloc ) )
+                        lattice::panel.lines( c( 0, input$xloc ), c( input$yloc, input$yloc ) )
+                    }
+                } )
+        }), ncol = 2 ) )
+        grid::grid.text( "Ferenci Tamás, 2019", 0, 0.02, gp = grid::gpar( fontface = "bold" ), just = "left" )
+        grid::grid.text( "http://research.physcon.", 1, 0.06, gp = grid::gpar( fontface = "bold" ), just = "right" )
+        grid::grid.text( "uni-obuda.hu", 1, 0.02, gp = grid::gpar( fontface = "bold" ), just = "right" )
+    }
+    
+    output$resultTable <- renderTable({
+        dat <- switch( input$type,
+                       "pretest" = data.frame( pretest = input$slider, sens = input$xloc, spec = input$yloc ),
+                       "sens" = data.frame( pretest = input$xloc, sens = input$slider, spec = input$yloc ),
+                       "spec" = data.frame( pretest = input$xloc, sens = input$yloc, spec = input$slider ) )
+        dat <- transform( dat,
+                          npv = ( spec*(100-pretest)/( (100-sens)*pretest + spec*(100-pretest) ) )*100,
+                          ppv = ( sens*pretest/( sens*pretest + (100-spec)*(100-pretest) ) )*100 )
+        dat <- reshape( dat, varying = 1:5, v.names = "Érték", timevar = "Paraméter", times = names( dat ), direction = "long" )
+        dat$`Paraméter` <- c( "Teszt előtti valószínűség [%]", "Szenzitivitás [%]", "Specificitás [%]",
+                              "Negatív prediktív érték [%]", "Pozitív prediktív érték [%]" )
+        dat[ , 1:2 ]
+    }, format.args = list( decimal.mark = "," ) )
+    
+    output$resultPlot <- renderPlot({
+        print( plotInput() )
+    })
+    
+    output$DownloadFigPDF <- downloadHandler(
+        filename = "TestPredictivePlot.pdf",
+        content = function( file ) {
+            cairo_pdf( file, width = 16, height = 9 )
+            print( plotInput() )
+            dev.off( )
+        } )
+    
+    output$DownloadFigPNG <- downloadHandler(
+        filename = "TestPredictivePlot.png",
+        content = function( file ) {
+            png( file, width = 1600, height = 900, type = "cairo-png" )
+            print( plotInput() )
+            dev.off( )
+        } )
+    
+}
+
+shinyApp( ui = ui, server = server )
